4ca0aefca316165749550ce0a84c000a7c071871
Orhan YILDIRIM <orhan_yil@msn.com>
2022-05-16 23:44:14 +0300

    Inheritence

--- BankAccount/main.cpp
+++ BankAccount/main.cpp
@@ -2,26 +2,28 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
 
-	bankAccount acc1("Ogun", "Birinci", 1500, "05071231212");
-	acc1.credit(5000);
+	/*person p1("Orhan", "Yildirim", "05423025705");
+	p1.print();
+
+	person p2 = p1;
+	p2.setPersonPhone("05455555452");
+	p2.print();*/
+
+	bankAccount acc1("Orhan", "Yildirim",1000, "05423025705");
+	acc1.setPersonBalance(2000);
 	acc1.displayProfile();
 
-
-	bankAccount acc2("Orhan", "Yildirim", 500, "05423025705");
+	bankAccount acc2 = acc1;
+	acc2.setPersonName("Burhan");
+	acc2.setPersonBalance(2500);
 	acc2.displayProfile();
 
-	bankAccount acc3 = acc1;
-	acc3.setPersonName("TestUser");
-	acc3.setPersonSurname("TestUserSurname");
+	acc1.sendMoney(acc2, 500);
+	acc1.displayProfile();
 
-	acc1.sendMoney(acc3, 5500);
-	acc3.displayProfile();
-
-	acc3.sendMoney(acc2, 11000);
-	acc2.displayProfile();
-
-
+	
 	return 0;
 }
